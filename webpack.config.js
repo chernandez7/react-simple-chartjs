bc5f3b46f4a5235e7aeb1863455581af39c7bae4
chernandez7 <chris00hernandez@gmail.com>
2019-03-21 11:46:10 -0500

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "simple-chart.min.js",
     library: "SimpleChart",
     libraryTarget: "umd"
-  },
-  externals: {
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "react",
-      root: "React"
-    }
   }
 };
